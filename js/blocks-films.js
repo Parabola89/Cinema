cc0fb5ff8fb65fc5005180d3cfadf4841baf0f71
Parabola89 <parabola89@list.tu>
2021-05-19 21:24:50 +0500

    1.17-19- DOM манипуляции

--- js/blocks-films.js
+++ js/blocks-films.js
@@ -23,10 +23,12 @@ fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FIL
         const id = `class9-films-desc-${film.filmId}`;
 
         blockFilmsWrapper.innerHTML += `
+        <div>
         <img src="${film.posterUrlPreview}">
         <div class="class9_films_text1">${film.nameRu}</div>    
             <div id="${id}" class="class9_films_text2">...loading</div>
-
+        </div>
+        
         `
     
 
